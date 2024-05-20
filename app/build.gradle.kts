ec78585da7caa8f83f01d4c589a31dc0b821d6c6
HunCY5 <cherrychoi35@gmail.com>
2024-05-20 15:51:05 +0900

    firestore 연동 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
 }
--- app/src/main/java/com/example/loginpage/JoinPage.kt
+++ app/src/main/java/com/example/loginpage/JoinPage.kt
@@ -12,11 +12,13 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 
 class JoinPage : AppCompatActivity() {
     lateinit var btn_back: Button
     private lateinit var auth: FirebaseAuth
     private lateinit var intent_back: Intent
+    private val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -36,14 +38,38 @@ class JoinPage : AppCompatActivity() {
         join.setOnClickListener {
             val email = findViewById<EditText>(R.id.et_email)
             val password = findViewById<EditText>(R.id.et_pw)
+            val nickname = findViewById<EditText>(R.id.et_nickname)
+            val age = findViewById<EditText>(R.id.et_age)
             auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        Toast.makeText(this, "회원가입 완료", Toast.LENGTH_LONG).show()
-//                        성공시, intent to 초기화면
-                        startActivity(intent_back)
+                        val user = auth.currentUser
+                        if(user != null)
+                        {
+                            val userData = hashMapOf(
+                                "uid" to user.uid,
+                                "ID" to email.text.toString(),
+                                "PW" to password.text.toString(),
+                                "Name" to nickname.text.toString(),
+                                "Age" to age.text.toString()
+                            )
 
-                    } else {
+                            db.collection("users")
+                                .document(user.uid)
+                                .set(userData)
+                                .addOnSuccessListener {
+                                    Toast.makeText(this, "회원가입 완료", Toast.LENGTH_LONG).show()
+//                        성공시, intent to 초기화면
+                                    startActivity(intent_back)
+                                }
+                                .addOnFailureListener { e ->
+                                    Toast.makeText(this, "Firestore 저장 실패: ${e.message}", Toast.LENGTH_LONG).show()
+                                }
+
+                        }
+
+                    }
+                    else {
                         Toast.makeText(this, "회원가입 실패", Toast.LENGTH_LONG).show()
                     }
                 }
